68d788addd37eccaf05a8a7078ca1dc982edeb6b
Saqeef Tehnan <stehnan@psu.edu.sa>
2023-01-17 08:43:48 +0300

    Made some changes in the script

--- scripts/custom_humble_install.sh
+++ scripts/custom_humble_install.sh
@@ -9,7 +9,6 @@ CHOOSE_ROS_DISTRO=humble # or humble, etc...
 INSTALL_PACKAGE=desktop # or ros-base
 
 #Set locale
-
 sudo apt update && sudo apt install locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
@@ -25,6 +24,16 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 
 #add the repository to your sources list.
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+#Install editors
+sudo apt update
+sudo apt install -y nano
+sudo apt install -y gedit
+#Install vscode
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code 
 
 #Install ROS 2 packages
 sudo apt update
