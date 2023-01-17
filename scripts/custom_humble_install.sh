b6f28919d2db7b0d15fda8f10a300984f6f951e3
Saqeef Tehnan <stehnan@psu.edu.sa>
2023-01-17 11:12:05 +0300

    Added gazebo in the script

--- scripts/custom_humble_install.sh
+++ scripts/custom_humble_install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-# Regular Colors
-Red='\033[0;31m'          # Red
-White='\033[0;37m'        # White
+#Install sudo 
+# su -
+apt-get update
+apt install sudo -y
 
 #Installation starts here
 CHOOSE_ROS_DISTRO=humble # or humble, etc...
@@ -30,10 +31,10 @@ sudo apt update
 sudo apt install -y nano
 sudo apt install -y gedit
 #Install vscode
-sudo apt install software-properties-common apt-transport-https wget -y
+sudo apt install -y software-properties-common apt-transport-https wget 
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt install code 
+sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code -y
 
 #Install ROS 2 packages
 sudo apt update
@@ -42,10 +43,31 @@ sudo apt install -y ros-$CHOOSE_ROS_DISTRO-$INSTALL_PACKAGE
 sudo apt install -y python3-argcomplete
 sudo apt install -y python3-colcon-common-extensions
 sudo apt install -y python3-rosdep python3-vcstool
+sudo apt install -y bash-completion command-not-found
+sudo apt install -y ros-humble-gazebo*
 
+export "source /etc/bash_completion" << ~/.bashrc
+source /opt/ros/humble/setup.bash
 
+echo "-----------------------------------------------------------"
+echo "-----------------------------------------------------------"
+echo "-----------------------------------------------------------"
+echo "-----------------------------------------------------------"
 echo "source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash" >> ~/.bashrc
 echo "-----------------------------------------------------------"
-echo "success installing ROS2 $CHOOSE_ROS_DISTRO"
 echo "-----------------------------------------------------------"
-echo "Have a good time with ${Red}ROS2 and don't forget to drop a ${White}star on my github for this repo  :')"
+echo "-----------------------------------------------------------"
+echo "Success installing ROS2 $CHOOSE_ROS_DISTRO"
+echo "-----------------------------------------------------------"
+echo "-----------------------------------------------------------"
+echo "-----------------------------------------------------------"
+echo "Have a good time with ROS2 and don't forget to drop a star on my github for this repo  :')"
+echo "-----------------------------------------------------------"
+echo "-----------------------------------------------------------"
+echo "-----------------------------------------------------------"
+echo "Don't forget to source to workspace before you start"
+echo "-----------------------------------------------------------"
+echo "-----------------------------------------------------------"
+echo "-----------------------------------------------------------"
+
+
